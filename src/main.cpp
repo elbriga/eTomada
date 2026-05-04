f1be105c4141d6eb8548450dd6f1c8622ad02536
elbriga <gabrielortizlour@gmail.com>
2026-05-04 11:14:23 -0300

    mostraMsg e handleClient

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,8 @@ IPAddress ipStr;
 // Set web server port number to 80
 WiFiServer server(80);
 
+// TODO :: Struct rele
 // Reles 1 a 8
-#define RELE_LUZ  1
 int pinosReles[8] = { 15, 13, 12, 14, -1,-1,-1,-1 };
 bool estadoReles[8] = { 0,0,0,0,0,0,0,0 };
 
@@ -35,6 +35,18 @@ const char* tzInfo = "<-03>3";
 
 
 // ==============================================================================================
+int timeoutMsg = 0;
+void mostraMsg(String msg, int timeout = 0) {
+  tft.clear();
+  tft.drawString(30, 0, "eTomada!");
+  tft.drawString(0, 20, msg);
+  tft.drawString(0, 40, ipStr.toString());
+  tft.display();
+
+  if (timeout > 0)
+    timeoutMsg = millis() + timeout;
+}
+
 void syncTime() {
   // Start NTP using the two servers above
   configTime(0, 0, ntpServer1, ntpServer2);
@@ -51,15 +63,6 @@ void syncTime() {
   }
 }
 
-void printTime(struct tm timeinfo, int px, int py) {
-  char formattedTime[80];
-  
-  //strftime(formattedTime, sizeof(formattedTime), "%A, %B %d %Y %H:%M:%S", &timeinfo);
-  strftime(formattedTime, sizeof(formattedTime), "%H:%M:%S", &timeinfo);
-
-  tft.drawString(px, py, String(formattedTime));
-}
-
 String controlaRele(int numRele, bool estado) {
   if (numRele < 1 || numRele > 8) {
     Serial.printf("controlaRele: numRele [%d] invalido!\n", numRele);
@@ -74,7 +77,7 @@ String controlaRele(int numRele, bool estado) {
     estadoReles[r] = estado;
 
     char msg[128];
-    sprintf(msg, "%s rele %d (%s) pino %d", (estado ? "Ligando" : "Desligando"), numRele, nomeReles[r].c_str(), pinosReles[r]);
+    sprintf(msg, "%s %s    (rele %d, pino %d)", (estado ? "Ligando" : "Desligando"), nomeReles[r].c_str(), numRele, pinosReles[r]);
     ret = msg;
   }
 
@@ -176,8 +179,115 @@ void onChangeMinute(tm timeinfo) {
     String msg = checkRegra(r, timeinfo);
     if (msg != "") {
       Serial.printf("[%d:%d] > %s\n", timeinfo.tm_hour, timeinfo.tm_min, msg.c_str());
+      mostraMsg(msg, 5000);
     }
   }
+}
+
+void handleClient(WiFiClient client, struct tm timeinfo) {
+  unsigned long currentTime = millis();
+  unsigned long previousTime = currentTime; 
+  // Define timeout time in milliseconds (example: 2000ms = 2s)
+  const long timeoutTime = 5000;
+
+  Serial.println("New Client.");
+
+  String currentLine = "";                // make a String to hold incoming data from the client
+  String header = "";
+  while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+    currentTime = millis();
+    if (client.available()) {             // if there's bytes to read from the client,
+      char c = client.read();             // read a byte, then
+      Serial.write(c);                    // print it out the serial monitor
+      header += c;
+      if (c == '\n') {                    // if the byte is a newline character
+        // if the current line is blank, you got two newline characters in a row.
+        // that's the end of the client HTTP request, so send a response:
+        if (currentLine.length() == 0) {
+          // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+          // and a content-type so the client knows what's coming, then a blank line:
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println("Connection: close");
+          client.println();
+          
+          /* turns the GPIOs on and off
+          if (header.indexOf("GET /26/on") >= 0) {
+            Serial.println("GPIO 26 on");
+            output26State = "on";
+            digitalWrite(output26, HIGH);
+          } else if (header.indexOf("GET /26/off") >= 0) {
+            Serial.println("GPIO 26 off");
+            output26State = "off";
+            digitalWrite(output26, LOW);
+          } else if (header.indexOf("GET /27/on") >= 0) {
+            Serial.println("GPIO 27 on");
+            output27State = "on";
+            digitalWrite(output27, HIGH);
+          } else if (header.indexOf("GET /27/off") >= 0) {
+            Serial.println("GPIO 27 off");
+            output27State = "off";
+            digitalWrite(output27, LOW);
+          }*/
+          
+          // Display the HTML web page
+          client.println("<!DOCTYPE html><html>");
+          client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+          // CSS to style the on/off buttons 
+          // Feel free to change the background-color and font-size attributes to fit your preferences
+          client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+          client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+          client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+          client.println(".button2 {background-color: #555555;}</style></head>");
+
+          char formattedTime[32];
+          strftime(formattedTime, sizeof(formattedTime), "%A, %B %d %Y %H:%M:%S", &timeinfo);
+          
+          // Web Page Heading
+          client.println(String("<body><h1>eTomada!</h1><br>") + String(formattedTime) + "<br><br>Reles:<br>");
+          
+          for (int r=0; r < 8; r++) {
+            if (regraReles[r] == "") continue;
+            client.println("<p>RELE " + String(r+1) + ": " + nomeReles[r] + " > " + regraReles[r] + " = " +
+                (estadoReles[r] ? "Ligado" : "Desligado") + "</p>");
+          }
+
+          /* Display current state, and ON/OFF buttons for GPIO 26  
+          // If the output26State is off, it displays the ON button       
+          if (output26State=="off") {
+            client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+          } else {
+            client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
+          } 
+              
+          // Display current state, and ON/OFF buttons for GPIO 27  
+          client.println("<p>GPIO 27 - State " + output27State + "</p>");
+          // If the output27State is off, it displays the ON button       
+          if (output27State=="off") {
+            client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
+          } else {
+            client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
+          }*/
+
+          client.println("</body></html>");
+          
+          // The HTTP response ends with another blank line
+          client.println();
+          // Break out of the while loop
+          break;
+        } else { // if you got a newline, then clear currentLine
+          currentLine = "";
+        }
+      } else if (c != '\r') {  // if you got anything else but a carriage return character,
+        currentLine += c;      // add it to the end of the currentLine
+      }
+    }
+  }
+
+  // Close the connection
+  client.stop();
+  Serial.println("Client disconnected.");
+  Serial.println("");
 }
 
 void setup() {
@@ -233,7 +343,7 @@ void setup() {
   Serial.println(ipStr);
   
   Serial.print("NTP: ");
-  tft.drawString(0, 40, "NTP Init...");
+  tft.drawString(0, 40, "Buscando Hora...");
   tft.display();
   syncTime();
   Serial.println("OK!");
@@ -244,12 +354,18 @@ void setup() {
   Serial.println("");
 }
 
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0; 
-// Define timeout time in milliseconds (example: 2000ms = 2s)
-const long timeoutTime = 2000;
+String getDiaSemana(struct tm timeinfo) {
+  switch (timeinfo.tm_wday) {
+    case 0: return "Dom";
+    case 1: return "Seg";
+    case 2: return "Ter";
+    case 3: return "Qua";
+    case 4: return "Qui";
+    case 5: return "Sex";
+    case 6: return "Sab";
+    default: return "---";
+  }
+}
 
 int lastMinute = -1;
 int lastSecond = -1;
@@ -260,13 +376,17 @@ void loop() {
     //return NULL;
   }
 
-  if (timeinfo.tm_sec != lastSecond) {
+  if (timeinfo.tm_sec != lastSecond && timeoutMsg < millis()) {
     lastSecond = timeinfo.tm_sec;
-    tft.clear();
-    tft.drawString(30, 0, "eTomada!");
-    printTime(timeinfo, 36, 20);
-    tft.drawString(0, 40, ipStr.toString());
-    tft.display();
+    timeoutMsg = 0;
+
+    // Atualizar o relogio
+    char formattedTime[10];
+    char msgDataHora[32];
+    //strftime(formattedTime, sizeof(formattedTime), "%A, %B %d %Y %H:%M:%S", &timeinfo);
+    strftime(formattedTime, sizeof(formattedTime), "%H:%M:%S", &timeinfo);
+    sprintf(msgDataHora, "  %s    %s", getDiaSemana(timeinfo).c_str(), formattedTime);
+    mostraMsg(msgDataHora);
   }
 
   if (timeinfo.tm_min != lastMinute) {
@@ -282,102 +402,7 @@ void loop() {
   delay(1);
 
   WiFiClient client = server.available();   // Listen for incoming clients
-
   if (client) {                             // If a new client connects,
-    currentTime = millis();
-    previousTime = currentTime;
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    String header = "";
-    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
-      currentTime = millis();
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            /* turns the GPIOs on and off
-            if (header.indexOf("GET /26/on") >= 0) {
-              Serial.println("GPIO 26 on");
-              output26State = "on";
-              digitalWrite(output26, HIGH);
-            } else if (header.indexOf("GET /26/off") >= 0) {
-              Serial.println("GPIO 26 off");
-              output26State = "off";
-              digitalWrite(output26, LOW);
-            } else if (header.indexOf("GET /27/on") >= 0) {
-              Serial.println("GPIO 27 on");
-              output27State = "on";
-              digitalWrite(output27, HIGH);
-            } else if (header.indexOf("GET /27/off") >= 0) {
-              Serial.println("GPIO 27 off");
-              output27State = "off";
-              digitalWrite(output27, LOW);
-            }*/
-            
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #555555;}</style></head>");
-            
-            // Web Page Heading
-            client.println("<body><h1>ESP32 Web Server</h1>Regras:");
-            
-            for (int r=0; r < 8; r++) {
-              if (regraReles[r] == "") continue;
-              client.println("<p>RELE " + String(r+1) + " > " + regraReles[r] + "</p>");
-            }
-
-            /* Display current state, and ON/OFF buttons for GPIO 26  
-            // If the output26State is off, it displays the ON button       
-            if (output26State=="off") {
-              client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
-               
-            // Display current state, and ON/OFF buttons for GPIO 27  
-            client.println("<p>GPIO 27 - State " + output27State + "</p>");
-            // If the output27State is off, it displays the ON button       
-            if (output27State=="off") {
-              client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }*/
-
-            client.println("</body></html>");
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
+    handleClient(client, timeinfo);
   }
 }
