ef7f523a1c5e380ee58280da283eb086fa8af0f0
elbriga <gabrielortizlour@gmail.com>
2026-05-17 19:58:22 -0300

    esp32dev sem display

--- include/display.h
+++ include/display.h
@@ -1,13 +1,5 @@
 #pragma once
 
-#include <SSD1306.h>
-
-#define I2C_DISPLAY_ADDR    0x3C
-#define SDA                 5
-#define SCL                 4
-
-extern SSD1306Wire tft;
-
 void displayInit();
 bool displayPodeMostrar();
 void displayMostraMsg(const char* msg, int timeout = 0);
--- src/display.cpp
+++ src/display.cpp
@@ -1,7 +1,13 @@
 #include "display.h"
 #include "wifi.h"
 
-// Display OLED
+#ifdef TEM_OLED
+
+#include <SSD1306Wire.h>
+#define I2C_DISPLAY_ADDR    0x3C
+#define SDA                 5
+#define SCL                 4
+
 SSD1306Wire tft(I2C_DISPLAY_ADDR, SDA, SCL);
 
 static unsigned long displayTimeoutMsg = 0;
@@ -29,11 +35,14 @@ void displayMostraString(int x, int y, const char *msg)
 void displayMostraMsg(const char* msg, int timeout)
 {
     tft.clear();
-    
+
     tft.drawString(30, 0, "eTomada!");
     tft.drawString(0, 20, msg);
-    
-    IPAddress ip = WiFiGetModoAP() ? WiFi.softAPIP() : WiFi.localIP();
+
+    IPAddress ip = WiFiGetModoAP()
+        ? WiFi.softAPIP()
+        : WiFi.localIP();
+
     tft.drawString(0, 40, ip.toString());
 
     tft.display();
@@ -42,3 +51,33 @@ void displayMostraMsg(const char* msg, int timeout)
         displayTimeoutMsg = millis() + timeout;
     }
 }
+
+#else
+
+void displayInit()
+{
+    Serial.println("[DISPLAY] OLED desabilitado");
+}
+
+bool displayPodeMostrar()
+{
+    return true;
+}
+
+void displayMostraString(int x, int y, const char *msg)
+{
+    Serial.printf("[DISPLAY] (%d,%d): %s\n", x, y, msg);
+}
+
+void displayMostraMsg(const char* msg, int timeout)
+{
+    Serial.printf("[DISPLAY] %s\n", msg);
+
+    IPAddress ip = WiFiGetModoAP()
+        ? WiFi.softAPIP()
+        : WiFi.localIP();
+
+    Serial.printf("[DISPLAY] IP: %s\n", ip.toString().c_str());
+}
+
+#endif
