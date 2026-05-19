74a986ad3ec8db02833eba741cf8c6a68411144e
elbriga <gabrielortizlour@gmail.com>
2026-05-19 17:34:37 -0300

    formattedUptime

--- src/loga.cpp
+++ src/loga.cpp
@@ -18,8 +18,29 @@ void logaMensagem(const char* fmt, ...) {
 
   char formattedTime[32] = {0};
   strftime(formattedTime, sizeof(formattedTime), "%d/%m/%Y %H:%M:%S", &timeinfo);
+  
+  char formattedUptime[32] = {0};
+  int uptime = millis() / 1000;
+  int dias = uptime / 86400;
+  int horas = (uptime % 86400) / 3600;
+  int minutos = (uptime % 3600) / 60;
+  int segundos = uptime % 60;
+  if (dias > 0) {
+    snprintf(formattedUptime, sizeof(formattedUptime), "%dd %s%d:%s%d:%s%d",
+      dias,
+      (horas < 10 ? "0" : ""), horas,
+      (minutos < 10 ? "0" : ""), minutos,
+      (segundos < 10 ? "0" : ""), segundos
+    );
+  } else {
+    snprintf(formattedUptime, sizeof(formattedUptime), "%s%d:%s%d:%s%d",
+      (horas < 10 ? "0" : ""), horas,
+      (minutos < 10 ? "0" : ""), minutos,
+      (segundos < 10 ? "0" : ""), segundos
+    );
+  }
 
-  Serial.printf("[%s][%d] %s\n", formattedTime, millis(), msg);
+  Serial.printf("[%s][%s] %s\n", formattedTime, formattedUptime, msg);
 }
 
 void logaTitulo(const char *msg) {
