b225c03682c54d09e052dbd6f978093e976429bf
elbriga <gabrielortizlour@gmail.com>
2026-05-23 13:03:49 -0300

    ren

---
+++ data/js/eTomada.js
@@ -0,0 +1,56 @@
+const API_BASE =
+  window.location.host === "localhost"
+    ? "http://192.168.18.105" // IP do ESP quando o frontend esta hospedado para DEV
+    : window.location.origin;
+
+let eTomadaData = null;
+
+function eTomadaInit() {
+  sseInit();
+  // Nao precisa Render aqui pois vem o evento sse_snapshot ao conectar
+  // eTomadaRender();
+}
+
+async function eTomadaRefresh(snapshot) {
+  if (!snapshot) {
+    snapshot = await eTomadaAPI("getSnapshot");
+  }
+
+  if (!snapshot.reles || !snapshot.sensores) {
+    statusMsg("Erro nos dados!");
+    return;
+  }
+
+  if (snapshot.api < 3) {
+    statusMsg("Erro versao API!");
+    return;
+  }
+
+  eTomadaData = snapshot;
+}
+
+let _eTomadaLoading = false;
+async function eTomadaRender(snapshot) {
+  statusMsg("");
+
+  if (_eTomadaLoading) return;
+  _eTomadaLoading = true;
+
+  try {
+    await eTomadaRefresh(snapshot);
+
+    document.getElementById("datahora").innerHTML =
+      "uptime: " +
+      formataTempo(eTomadaData.uptime) +
+      " - " +
+      eTomadaData.datahorastr;
+
+    sensoresRender(eTomadaData.sensores);
+
+    relesRender(eTomadaData.reles);
+  } catch (e) {
+    statusMsg("Erro eTomadaRender: " + e);
+  } finally {
+    _eTomadaLoading = false;
+  }
+}
