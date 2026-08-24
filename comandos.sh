43b5f378f50601a77360ef41d6b6fc84d5ea7967
elbriga <gabrielortizlour@gmail.com>
2026-08-24 09:15:09 -0300

    Backtrace

--- comandos.sh
+++ comandos.sh
@@ -11,3 +11,6 @@ scp .pio/build/lolin/firmware.bin pi:/root/nginx-docker/html/firmware/lolin32_R6
 
 # Umidificador!
 curl -v -X PUT "http://192.168.1.151/api/setUmidificador" -H "Content-Type: application/json" -d '{"estado":0}'
+
+# Backtrace
+~/.platformio/packages/toolchain-xtensa-esp32/bin/xtensa-esp32-elf-addr2line -pfiaC -e .pio/build/lolin/firmware.elf 0x4008428d:0x3ffb2c50 0x4008f309:0x3ffb2c70 ...
