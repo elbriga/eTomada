861bf7eb1d84bb16819e369523d263a4e3f34660
elbriga <gabrielortizlour@gmail.com>
2026-08-30 19:46:25 -0300

    cmds

--- comandos.sh
+++ comandos.sh
@@ -25,7 +25,7 @@ curl -v -X PUT "http://192.168.1.151/api/setUmidificador" -H "Content-Type: appl
 # sonoff
 # Avahi?
 https://www.sigmdel.ca/michel/ha/sonoff/sonoff_mini_en.html
-avahi-browse -t _ewelink._tcp --resolve
+avahi-browse -t _etomada._tcp --resolve
 # API
 curl -v http://192.168.1.115:8081/zeroconf/info -H "Content-Type: application/json" -d '{"deviceid":"1000c8797e","data":{}}'
 
